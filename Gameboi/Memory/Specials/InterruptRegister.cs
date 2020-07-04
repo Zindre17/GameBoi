3254cc9123e87dc994ce503e4e8a13932c9b0cd1
Sindre Aarstrand <saarstrand@akvagroup.com>
2020-07-05 00:45:05 +0200

    Bugfix in interrupt register

--- Gameboi/Memory/Specials/InterruptRegister.cs
+++ Gameboi/Memory/Specials/InterruptRegister.cs
@@ -9,28 +9,28 @@ class InterruptRegister : MaskedRegister
     public bool Vblank
     {
         get => TestBit(0, data);
-        set => data = value ? SetBit(0, data) : ResetBit(0, data);
+        set => Write(value ? SetBit(0, data) : ResetBit(0, data));
     }
 
     public bool LcdStat
     {
         get => TestBit(1, data);
-        set => data = value ? SetBit(1, data) : ResetBit(1, data);
+        set => Write(value ? SetBit(1, data) : ResetBit(1, data));
     }
     public bool Timer
     {
         get => TestBit(2, data);
-        set => data = value ? SetBit(2, data) : ResetBit(2, data);
+        set => Write(value ? SetBit(2, data) : ResetBit(2, data));
     }
     public bool Serial
     {
         get => TestBit(3, data);
-        set => data = value ? SetBit(3, data) : ResetBit(3, data);
+        set => Write(value ? SetBit(3, data) : ResetBit(3, data));
     }
     public bool Joypad
     {
         get => TestBit(4, data);
-        set => data = value ? SetBit(4, data) : ResetBit(4, data);
+        set => Write(value ? SetBit(4, data) : ResetBit(4, data));
     }
 
 }
