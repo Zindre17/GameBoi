19764a727ea40874c20d84aad3e5b95167c3ce5f
Sindre Aarstrand <zindre17@gmail.com>
2020-07-12 20:22:34 +0200

    Simplified InterruptRegister

--- Gameboi/Memory/Specials/InterruptRegister.cs
+++ Gameboi/Memory/Specials/InterruptRegister.cs
@@ -4,32 +4,32 @@ class InterruptRegister : MaskedRegister
 {
     public InterruptRegister() : base(0xE0) { }
 
-    public bool Any() => (data & 0x1F) != 0;
+    public bool Any() => data != mask;
 
     public bool Vblank
     {
-        get => TestBit(0, data);
+        get => data[0];
         set => Write(value ? SetBit(0, data) : ResetBit(0, data));
     }
 
     public bool LcdStat
     {
-        get => TestBit(1, data);
+        get => data[1];
         set => Write(value ? SetBit(1, data) : ResetBit(1, data));
     }
     public bool Timer
     {
-        get => TestBit(2, data);
+        get => data[2];
         set => Write(value ? SetBit(2, data) : ResetBit(2, data));
     }
     public bool Serial
     {
-        get => TestBit(3, data);
+        get => data[3];
         set => Write(value ? SetBit(3, data) : ResetBit(3, data));
     }
     public bool Joypad
     {
-        get => TestBit(4, data);
+        get => data[4];
         set => Write(value ? SetBit(4, data) : ResetBit(4, data));
     }
 
