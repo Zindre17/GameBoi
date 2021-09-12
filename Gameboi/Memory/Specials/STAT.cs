0dab932cbb860756d7cd2e5098545d06921d295e
Sindre Aarstrand <zindre17@gmail.com>
2021-09-12 12:19:42 +0200

    Refactor: cleaned up STAT register

--- Gameboi/Memory/Specials/STAT.cs
+++ Gameboi/Memory/Specials/STAT.cs
@@ -7,10 +7,10 @@ namespace GB_Emulator.Gameboi.Memory.Specials
     {
         public STAT() : base(0x80) { }
 
-        public bool IsCoincidenceInterruptEnabled { get => data[6]; set => Write(value ? SetBit(6, data) : ResetBit(6, data)); }
-        public bool IsOAMInterruptEnabled { get => data[5]; set => Write(value ? SetBit(5, data) : ResetBit(5, data)); }
-        public bool IsVblankInterruptEnabled { get => data[4]; set => Write(value ? SetBit(4, data) : ResetBit(4, data)); }
-        public bool IsHblankInterruptEnabled { get => data[3]; set => Write(value ? SetBit(3, data) : ResetBit(3, data)); }
+        public bool IsCoincidenceInterruptEnabled => data[6];
+        public bool IsOAMInterruptEnabled => data[5];
+        public bool IsVblankInterruptEnabled => data[4];
+        public bool IsHblankInterruptEnabled => data[3];
         public bool CoincidenceFlag { get => data[2]; set => base.Write(value ? SetBit(2, data) : ResetBit(2, data)); }
 
         public override void Write(Byte value) => base.Write(value & 0xF8 | data & 0x07);
