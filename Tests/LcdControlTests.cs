7f7524b1fe5cd06d318bc6917223b7e540f9adb5
Sindre Aarstrand <zindre17@gmail.com>
2023-03-22 20:27:22 +0100

    Added LcdControl tests

---
+++ Tests/LcdControlTests.cs
@@ -0,0 +1,82 @@
+using Gameboi.Memory.Io;
+
+namespace Tests;
+
+[TestClass]
+public class LcdControlTests
+{
+    [TestMethod]
+    public void IsLcdEnabled()
+    {
+        TestFlag(control => control.IsLcdEnabled, 7);
+    }
+
+    [TestMethod]
+    public void WindowUsesHighTileMapArea()
+    {
+        TestFlag(control => control.WindowUsesHighTileMapArea, 6);
+    }
+
+    [TestMethod]
+    public void IsWindowEnabled()
+    {
+        TestFlag(control => control.IsWindowEnabled, 5);
+    }
+
+    [TestMethod]
+    public void BackgroundAndWindowUsesLowTileDataArea()
+    {
+        TestFlag(control => control.BackgroundAndWindowUsesLowTileDataArea, 4);
+    }
+
+    [TestMethod]
+    public void BackgroundUsesHighTileMapArea()
+    {
+        TestFlag(control => control.BackgroundUsesHighTileMapArea, 3);
+    }
+
+    [TestMethod]
+    public void IsDoubleSpriteSize()
+    {
+        TestFlag(control => control.IsDoubleSpriteSize, 2);
+    }
+
+    [TestMethod]
+    public void IsSpritesEnabled()
+    {
+        TestFlag(control => control.IsSpritesEnabled, 1);
+    }
+
+    [TestMethod]
+    public void IsBackgroundEnabled()
+    {
+        TestFlag(control => control.IsBackgroundEnabled, 0);
+    }
+
+    private static void TestFlag(Func<LcdControl, bool> selector, int expectedBitPosition)
+    {
+        var status = new LcdControl(1 << 0);
+        Assert.AreEqual(expectedBitPosition is 0, selector(status));
+
+        status = new LcdControl(1 << 1);
+        Assert.AreEqual(expectedBitPosition is 1, selector(status));
+
+        status = new LcdControl(1 << 2);
+        Assert.AreEqual(expectedBitPosition is 2, selector(status));
+
+        status = new LcdControl(1 << 3);
+        Assert.AreEqual(expectedBitPosition is 3, selector(status));
+
+        status = new LcdControl(1 << 4);
+        Assert.AreEqual(expectedBitPosition is 4, selector(status));
+
+        status = new LcdControl(1 << 5);
+        Assert.AreEqual(expectedBitPosition is 5, selector(status));
+
+        status = new LcdControl(1 << 6);
+        Assert.AreEqual(expectedBitPosition is 6, selector(status));
+
+        status = new LcdControl(1 << 7);
+        Assert.AreEqual(expectedBitPosition is 7, selector(status));
+    }
+}
