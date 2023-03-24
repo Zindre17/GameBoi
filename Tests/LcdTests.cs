fafe98216e1c9b6e6566976f446b3f09bbdb3dcb
Sindre Aarstrand <zindre17@gmail.com>
2023-03-24 23:00:20 +0100

    Added lcd tests

--- Tests/LcdTests.cs
+++ Tests/LcdTests.cs
@@ -1,4 +1,7 @@
+using Gameboi;
 using Gameboi.Hardware;
+using Gameboi.Memory.Io;
+using static Gameboi.Hardware.LcdConstants;
 
 namespace Tests;
 
@@ -56,5 +59,77 @@ public class LcdTests
 
         lcd.Update(1, 1);
         Assert.AreNotEqual(mode, lcd.Mode);
+    }
+}
+
+[TestClass]
+public class ImprovedLcdTests
+{
+    private readonly SystemState state;
+    private readonly ImprovedLcd lcd;
+
+    public ImprovedLcdTests()
+    {
+        state = new();
+        lcd = new(state);
+        state.Reset(false);
+    }
+
+    [TestMethod]
+    public void ModeDurations()
+    {
+        state.LineY = 0;
+        state.LcdStatus = new LcdStatus(0).WithMode(SearchingOam);
+        state.LcdRemainingTicksInMode = SearchingOamDurationInTicks;
+
+        for (var i = 0; i < VerticalBlankLineYStart; i++)
+        {
+            // Mode 2 OAM search
+            if (i is not 0)
+            {
+                lcd.Tick();
+            }
+            Assert.AreEqual(SearchingOam, new LcdStatus(state.LcdStatus).Mode);
+
+            for (var ticks = 0; ticks < SearchingOamDurationInTicks - 1; ticks++)
+            {
+                lcd.Tick();
+                Assert.AreEqual(SearchingOam, new LcdStatus(state.LcdStatus).Mode, message: $"{i}: {ticks}");
+            }
+
+            // Mode 3 Transfer data to lcd (for line)
+            lcd.Tick();
+            Assert.AreEqual(TransferringDataToLcd, new LcdStatus(state.LcdStatus).Mode, message: $"{i}");
+
+            for (var ticks = 0; ticks < GeneratePixelLineDurationInTicks - 1; ticks++)
+            {
+                lcd.Tick();
+                Assert.AreEqual(TransferringDataToLcd, new LcdStatus(state.LcdStatus).Mode, message: $"{i}: {ticks}");
+            }
+
+            // Mode 0 H-blank
+            lcd.Tick();
+            Assert.AreEqual(HorizontalBlank, new LcdStatus(state.LcdStatus).Mode, message: $"{i}");
+
+            for (var ticks = 0; ticks < HorizontalBlankDurationInTicks - 1; ticks++)
+            {
+                lcd.Tick();
+                Assert.AreEqual(HorizontalBlank, new LcdStatus(state.LcdStatus).Mode, message: $"{i}: {ticks}");
+            }
+        }
+
+        lcd.Tick();
+        Assert.AreEqual(VerticalBlank, new LcdStatus(state.LcdStatus).Mode);
+
+        // Mode 1 V-blank
+        for (var i = 0; i < VerticalBlankDurationInTicks - 1; i++)
+        {
+            lcd.Tick();
+            Assert.AreEqual(VerticalBlank, new LcdStatus(state.LcdStatus).Mode, message: $"{i}");
+            Assert.AreEqual(((i + 1) / ScanLineDurationInTicks) + VerticalBlankLineYStart, state.LineY, message: $"{i}");
+        }
+
+        lcd.Tick();
+        Assert.AreEqual(SearchingOam, new LcdStatus(state.LcdStatus).Mode);
     }
 }
