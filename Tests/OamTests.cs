54a433a1f646ce8e633a810e9e046142172d05a3
Sindre Aarstrand <zindre17@gmail.com>
2023-03-24 20:15:32 +0100

    Added oam tests

---
+++ Tests/OamTests.cs
@@ -0,0 +1,24 @@
+using Gameboi.Graphics;
+
+namespace Tests;
+
+[TestClass]
+public class OamTests
+{
+    private readonly byte[] oam = new byte[0xa0];
+
+    [TestMethod]
+    public void GetSprites()
+    {
+        byte number = 0;
+        foreach (var sprite in ImprovedOam.GetSprites(oam))
+        {
+            oam[number * 4] = number;
+            oam[(number * 4) + 1] = (byte)(number * 2);
+            Assert.AreEqual(number, sprite.SpriteNr);
+            Assert.AreEqual(number, sprite.Y);
+            Assert.AreEqual(number * 2, sprite.X);
+            number++;
+        }
+    }
+}
