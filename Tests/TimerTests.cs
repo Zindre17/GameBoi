af62ce6e9adaadd36d1c4860ee877a769e91894c
Sindre Aarstrand <zindre17@gmail.com>
2023-03-26 19:40:24 +0200

    Add timer tests

---
+++ Tests/TimerTests.cs
@@ -0,0 +1,94 @@
+using Gameboi;
+using Gameboi.Hardware;
+
+namespace Tests;
+
+[TestClass]
+public class TimerTests
+{
+    private readonly SystemState state = new();
+
+    [TestMethod]
+    public void Tick()
+    {
+        var timer = new ImprovedTimer(state);
+
+        state.Div = 0;
+        state.Tima = 0;
+        state.Tma = 40;
+
+        // enable timer & use same increment speed as div;
+        state.Tac = 7;
+
+        for (var i = 0; i < 255; i++)
+        {
+
+            timer.Tick();
+
+            Assert.AreEqual(0, state.Div);
+            Assert.AreEqual(0, state.Tima);
+        }
+
+
+        timer.Tick();
+
+        Assert.AreEqual(1, state.Div);
+        Assert.AreEqual(1, state.Tima);
+
+        for (var i = 0; i < (256 * 0xff) - 1; i++)
+        {
+
+            timer.Tick();
+        }
+        Assert.AreEqual(0xff, state.Div);
+        Assert.AreEqual(0xff, state.Tima);
+
+
+        timer.Tick();
+
+        Assert.AreEqual(0, state.Div);
+        Assert.AreEqual(40, state.Tima);
+
+        state.Tac = 4;
+
+        for (var i = 0; i < 1023; i++)
+        {
+
+            timer.Tick();
+
+            Assert.AreEqual(40, state.Tima);
+        }
+
+        timer.Tick();
+
+        Assert.AreEqual(41, state.Tima);
+
+        state.Tac = 5;
+
+        for (var i = 0; i < 15; i++)
+        {
+
+            timer.Tick();
+
+            Assert.AreEqual(41, state.Tima);
+        }
+
+        timer.Tick();
+
+        Assert.AreEqual(42, state.Tima);
+
+        state.Tac = 6;
+
+        for (var i = 0; i < 63; i++)
+        {
+
+            timer.Tick();
+
+            Assert.AreEqual(42, state.Tima);
+        }
+
+        timer.Tick();
+
+        Assert.AreEqual(43, state.Tima);
+    }
+}
