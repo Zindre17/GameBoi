fe947b139d2eaf567aa24532032e97a10309c961
Sindre Aarstrand <saarstrand@akvagroup.com>
2024-02-05 21:15:39 +0100

    Stop branches when entering RAM for program execution or when outside bus range

--- Tools/RomDecompiler.cs
+++ Tools/RomDecompiler.cs
@@ -16,16 +16,17 @@ internal class RomDecompiler
     private readonly HashSet<int> visitedAddresses = new();
     private State state = State.Stopped;
     private readonly FileStream file;
+    private Branch currentBranch = null!;
 
     private bool InProgress => state is State.Reading || branches.Any();
 
-    private void AddBranch(int address, string label)
+    private void AddBranch(int address, string label, string? comment = null)
     {
         if (visitedAddresses.Contains(address))
         {
             return;
         }
-        branches.Push(new(address, label));
+        branches.Push(new(address, label, comment));
     }
     private Branch TakeOutNextBranch() => branches.Pop();
 
@@ -65,17 +66,48 @@ internal class RomDecompiler
                 StartReadingNextBranch();
             }
 
-            var position = file.Position;
+            var position = (int)file.Position;
+
+            if (IsRamAddressArea(position))
+            {
+                Console.WriteLine($"0x{position:X4}: RAM area. Aborting branch.");
+                state = State.Stopped;
+                continue;
+            }
+
+            if (IsOutOfBusRange(position))
+            {
+                Console.WriteLine($"0x{position:X4}: Out of bus range");
+                state = State.Stopped;
+                continue;
+            }
+
             var opCode = ReadOpCode();
+
+            if (IsProbablyUnusedMemory(position, opCode))
+            {
+                Console.WriteLine("Likeley bug in decompiler (if not an interrupt vector). 0xFF (restart 0x38) is default rom value for unused memory. Stopping.");
+                state = State.Stopped;
+                continue;
+            }
+
             var argument = ReadArgument(opCode);
 
             OutputDecompiledOperation(position, opCode, argument);
+
+            if (IsCall(opCode))
+            {
+                if (argument is Argument arg)
+                {
+                    AddBranch(arg.Value, currentBranch.Label, "Called from 0x" + position.ToString("X4"));
+                }
+            }
 
             if (IsRelativeJump(opCode))
             {
                 if (argument is Argument arg)
                 {
-                    AddBranch((int)(arg.Value + file.Position), "Jump from 0x" + position.ToString("X4"));
+                    AddBranch((int)(arg.Value + file.Position), currentBranch.Label, "Jumped from 0x" + position.ToString("X4"));
                 }
                 state = State.Stopped;
             }
@@ -84,10 +116,11 @@ internal class RomDecompiler
             {
                 if (argument is Argument arg)
                 {
-                    AddBranch(arg.Value, "Jump from 0x" + position.ToString("X4"));
+                    AddBranch(arg.Value, currentBranch.Label, "Jump from 0x" + position.ToString("X4"));
                 }
                 state = State.Stopped;
             }
+
             if (IsReturn(opCode))
             {
                 state = State.Stopped;
@@ -95,7 +128,7 @@ internal class RomDecompiler
         }
     }
 
-    private static void OutputDecompiledOperation(long position, int opCode, IArgument argument)
+    private static void OutputDecompiledOperation(int position, int opCode, IArgument argument)
     {
         var assemblyString = AssemblyConverter.ToString(opCode, argument);
         Console.WriteLine($"0x{position:X4}: 0x{opCode:X2} - {assemblyString}");
@@ -132,9 +165,9 @@ internal class RomDecompiler
 
     private void StartReadingNextBranch()
     {
-        var branch = TakeOutNextBranch();
-        Console.WriteLine($"\n--------- {branch.Label} ---------");
-        file.Position = branch.Address;
+        currentBranch = TakeOutNextBranch();
+        Console.WriteLine($"\n--------- {currentBranch} ---------");
+        file.Position = currentBranch.Address;
         state = State.Reading;
     }
 
@@ -145,8 +178,16 @@ internal class RomDecompiler
     }
 
     private static bool IsRelativeJump(int opCode) => opCode is 0x18;
-    private static bool IsAbsoluteJump(int opCode) => opCode is 0xc3 or 0xc7 or 0xcf or 0xd7 or 0xdf or 0xe7 or 0xe9 or 0xef or 0xf7 or 0xff;
+    private static bool IsAbsoluteJump(int opCode) => opCode is 0xc3 or 0xcf or 0xe9;
     private static bool IsReturn(int opCode) => opCode is 0xc9 or 0xd9;
+    private static bool IsCall(int opCode) => opCode is 0xc4 or 0xc7 or 0xcc or 0xcd or 0xcf or 0xd4 or 0xd7 or 0xdc or 0xdf or 0xe7 or 0xef or 0xf7 or 0xff;
+    private static bool IsRamAddressArea(int address) => address is >= 0x8000;
+    private static bool IsOutOfBusRange(int address) => address is < 0 or > 0xffff;
+    private bool IsProbablyUnusedMemory(int address, int opCode) => IsStartOfBranch(address) && opCode is 0xff;
+    private bool IsStartOfBranch(int address) => address == currentBranch.Address;
 }
 
-internal record Branch(int Address, string Label);
+internal record Branch(int Address, string Label, string? Comment = null)
+{
+    public override string ToString() => Comment is null ? Label : $"{Label}: {Comment}";
+};
